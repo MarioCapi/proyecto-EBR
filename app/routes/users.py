6544fe4bda866702556c0df7500a37ba3328cc6f
Mario Barragan <mariocapi90mym@gmail.com>
2024-12-30 11:39:34 -0500

    Se crea el usuario con contraseña cifrada y el metodo la devuelve junto con el email en el retur para enviar al usuario

--- app/routes/users.py
+++ app/routes/users.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import Session
 from utils.config.connection import get_db
 from utils.exec_any_SP_SQLServer import ejecutar_procedimiento, ejecutar_procedimiento_read
 from typing import Optional
-from datetime import datetime
+#from datetime import datetime
+import bcrypt
+import secrets
+import string
 
 class UserBase(BaseModel):
     company_id: int
@@ -12,9 +15,14 @@ class UserBase(BaseModel):
     first_name: str
     last_name: str = ""  # Valor por defecto
     role_id: int = 4  # ID del rol "Analista contable y financiero"
-    password_hash: str = "default_password_hash"  # Esto debería ser un hash real en producción
+    #password_hash: str = "default_password_hash"  # Esto debería ser un hash real en producción
 
 users_router = APIRouter()
+
+def generate_secure_password(length=8):
+    characters = string.ascii_letters + string.digits + "!@#$%^&*"
+    password = ''.join(secrets.choice(characters) for _ in range(length))
+    return password
 
 #@users_router.post("/users/create-from-company")
 async def create_user_from_company(
@@ -24,6 +32,13 @@ async def create_user_from_company(
     db):
     print(f"Iniciando creación de usuario para compañía {company_id}")
     try:
+        # Generar una única contraseña
+        password = generate_secure_password()
+        
+        # Crear el hash de la misma contraseña
+        salt = bcrypt.gensalt()
+        password_hash = bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
+
         user_data = UserBase(
             company_id=company_id,
             email=email,
@@ -37,7 +52,7 @@ async def create_user_from_company(
             'first_name': user_data.first_name,
             'last_name': user_data.last_name,
             'email': user_data.email,
-            'password_hash': user_data.password_hash,
+            'password_hash': password,
             'role_id': user_data.role_id,
             'active': 1
         }
@@ -56,7 +71,9 @@ async def create_user_from_company(
             )
 
         return {
-            "message": "Usuario creado exitosamente"
+            "message": "Usuario creado exitosamente",
+            "email": email,
+            "temporary_password": password
         }
     except Exception as e:
         print(f"Error detallado en crear usuario: {str(e)}")
